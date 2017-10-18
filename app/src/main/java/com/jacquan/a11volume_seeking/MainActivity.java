ce283c4874e552ea1b605ab44d5ed251a117f5d1
Jacquan McClinton <nauqcaj@gmail.com>
2017-10-18 11:45:17 -0500

    Initial commit

---
+++ app/src/main/java/com/jacquan/a11volume_seeking/MainActivity.java
@@ -0,0 +1,72 @@
+package com.jacquan.a11volume_seeking;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    AudioManager audioManager;
+
+    MediaPlayer mediaPlayer;
+
+    public void playAudio(View view){
+        mediaPlayer.start();
+    }
+
+    public void pauseAudio(View view){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.laugh);
+
+
+        audioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        //for max volume
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        //for current volume
+
+        int currentVolume= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        final SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+
+        volumeControl.setMax(maxVolume);
+
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(audioManager.STREAM_MUSIC, i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+    }
+}
